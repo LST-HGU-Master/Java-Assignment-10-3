377fcd0e9f44690e1c91e2a0f360a27ff47d2f9d
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-09 12:43:31 +0900

    Update ProgA3Test.java

--- src/test/java/ProgA3Test.java
+++ src/test/java/ProgA3Test.java
@@ -1,7 +1,9 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import java.io.*;
-
+/**
+ * @version (20220609)
+ **/
 public class ProgA3Test {
 
     @Test
@@ -18,14 +20,12 @@ public class ProgA3Test {
         // in.inputln("2"); // 標準入力をテストする場合
         // Hello.main(new String[]{"1", "2", "3"}); // 実行時引数をテストする場合
         ProgA3.main(new String[]{"12"});
-
-        // assertion
-        String[] prints = bos.toString().split("\n");
-        assertEquals(2*12, prints.length);
-        assertEquals("工太はスライムAに攻撃した！", prints[0]);
-        assertEquals("工太はスライムLに攻撃した！", prints[prints.length - 2]);
-
         // undo the binding in System
         System.setOut(originalOut);
+        // assertion
+        String[] prints = bos.toString().split("\r\n|\n", -1);
+        assertEquals(2*12, prints.length-1);
+        assertEquals("工太はスライムAに攻撃した！", prints[0]);
+        assertEquals("工太はスライムLに攻撃した！", prints[prints.length - 3]);
     }
 }
